5d849622484afe49cb98524d138bba308de4dcc3
senpai0666 <50199317+senpai0666@users.noreply.github.com>
2021-10-18 22:11:09 +0530

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Numbers</h2>
+
+<p>If you add two numeric strings, the result will be a concatenated string:</p>
+
+<p id="demo"></p>
+
+<script>
+let x = "10";
+let y = "20";
+let z = x + y;
+document.getElementById("demo").innerHTML = z;
+</script>
+
+</body>
+</html>
+
+
